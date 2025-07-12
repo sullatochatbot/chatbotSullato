3f1d900d17ce181a943446848b48f8e7c7f0cea4
sullatochatbot <andersonsullato@gmail.com>
2025-07-12 13:09:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return "Chatbot Sullato online com Flask!"
 
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        if token == "sullatotoken123":
+            return challenge
+        return "Token inválido"
+
+    if request.method == "POST":
+        data = request.get_json()
+        print("Mensagem recebida:", data)
+        return "Evento recebido", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
